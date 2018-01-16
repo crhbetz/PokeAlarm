4227586e2c8709924ea31de80110dd521a531fc1
Christian Betz <christian@betz.cc>
2018-01-16 10:42:10 +0100

    LOCAL ONLY: Errors with telegram chat_id
    Need to see chat_id on Telegram sending issues because users can block the bot, which leads to severe local lag.

--- PokeAlarm/Alarms/Alarm.py
+++ PokeAlarm/Alarms/Alarm.py
@@ -93,9 +93,9 @@ class Alarm(object):
                 log.error("Encountered error while sending notification"
                           + " ({}: {})".format(type(e).__name__, e))
                 log.debug("Stack trace: \n {}".format(traceback.format_exc()))
-                log.info(
-                    "{} is having connection issues. {} attempt of {}.".format(
-                        name, i + 1, max_attempts))
+                log.warning(
+                    "{} is having connection issues. {} attempt of {}. User: {}".format(
+                        name, i + 1, max_attempts, args["chat_id"]))
                 time.sleep(3)
                 reconnect()
         log.error("Could not send notification... Giving up.")
